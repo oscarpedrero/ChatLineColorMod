63a52dc4c85f059aea09c47864b13316e1aef932
Trodi <oscarpedrero@gmail.com>
2022-07-13 00:34:50 +0200

    Agregar archivos de proyecto.

---
+++ Hooks/ChatColor.cs
@@ -0,0 +1,71 @@
+﻿using HarmonyLib;
+using ProjectM.Network;
+using ProjectM.UI;
+using Unity.Entities;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace ChatLineColorMod.Hooks;
+
+[HarmonyPatch]
+internal class HUDChatWindow_Path
+{
+
+    private static TMP_InputField chatLine = null;
+
+    private static Color colorLocal = new(218 / 255f, 201 / 255f, 192 / 255f);
+    private static Color colorGLobal = new(153 / 255f, 204 / 255f, 255 / 255f);
+    private static Color colorTeam = new(121 / 255f, 218 / 255f, 166 / 255f);
+    private static Color colorWhisper = new(255 / 255f, 159 / 255f, 255 / 255f);
+    private static Color colorSystem = new(228 / 255f, 141 / 255f, 40 / 255f);
+    private static string lastChannel = "Local";
+
+    [HarmonyPatch(typeof(HUDChatWindow), nameof(HUDChatWindow.FocusInputField))]
+    [HarmonyPrefix]
+    public static void FocusInputField_Prefix(HUDChatWindow __instance)
+    {
+            chatLine = __instance.ChatInputField;
+    }
+
+    [HarmonyPatch(typeof(ClientChatSystem), nameof(ClientChatSystem.OnUpdate))]
+    [HarmonyPrefix]
+    public static void CheckedState_Prefix(ClientChatSystem __instance)
+    {
+
+        var channel = __instance._DefaultMode.ToString();
+
+        lastChannel = channel;
+
+        if (chatLine != null)
+        {
+            if (channel == "Local")
+            {
+               
+                chatLine.textComponent.color = colorLocal;
+            }
+            else if (channel == "Global")
+            {
+                
+                chatLine.textComponent.color = colorGLobal;
+            }
+            else if (channel == "Team")
+            {
+                
+                chatLine.textComponent.color = colorTeam;
+            }
+            else if (channel == "Whisper")
+            {
+                
+                chatLine.textComponent.color = colorWhisper;
+            }
+            else if (channel == "System")
+            {
+                chatLine.textComponent.color = colorSystem;
+            }
+        }
+
+        
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using Wetstone.API;
+
+namespace ChatLineColorMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("xyz.molenzwiebel.wetstone")]
+    [Reloadable]
+    public class Plugin : BasePlugin
+    {
+        public static ManualLogSource Logger;
+
+        private Harmony _harmony;
+
+        public override void Load()
+        {
+            Logger = Log;
+            _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public override bool Unload()
+        {
+            _harmony.UnpatchSelf();
+            return true;
+        }
+    }
+}
